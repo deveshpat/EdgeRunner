f3e91fe3f2e2e2962d823c84b5bc2ed8c845f900
Devesh Patel <63148075+deveshpat@users.noreply.github.com>
2026-07-09 08:00:59 +0000

    package

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  // basePath: "/EdgeRunner", // Note: Uncomment this when you deploy to GitHub Pages
+  basePath: "/EdgeRunner", // Note: Uncomment this when you deploy to GitHub Pages
 };
 
 export default nextConfig;
